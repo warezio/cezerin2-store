345326992bbf732886cb14182e2a68ff279740d0
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-28 13:01:30 +0700

    Update

--- src/components/theme/src/components/resetPassword/index.tsx
+++ src/components/theme/src/components/resetPassword/index.tsx
@@ -1,69 +1,67 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-import { themeSettings, text } from '../../lib/settings';
-import ResetPassword from './resetPassword';
+import React, { useState } from "react"
+import { Redirect } from "react-router-dom"
+import { themeSettings, text } from "../../lib/settings"
+import ResetPassword from "./resetPassword"
 
-const ResetPasswordForm = () =>{
-		this.state = {
-			verifiedToken: false
-		};
+const ResetPasswordForm = props => {
+  const [verifiedToken, setVerifiedTocken] = useState(false)
 
-	verifyToken() {
-		this.setState({ verifiedToken: true });
-		this.props.resetPassword({
-			token: this.props.location.search.split('=')[1]
-		});
-	}
+  function verifyToken() {
+    setVerifiedToken(true)
+    props.resetPassword({
+      token: props.location.search.split("=")[1],
+    })
+  }
 
-	handleFormSubmit = values => {
-		const userId = this.props.state.resetPasswordProperties.id;
-		this.props.resetPassword({
-			id: userId,
-			password: values.password,
-			history: this.props.history
-		});
-	};
-		!this.state.verifiedToken ? this.verifyToken() : '';
+  const handleFormSubmit = values => {
+    const userId = this.props.state.resetPasswordProperties.id
+    this.props.resetPassword({
+      id: userId,
+      password: values.password,
+      history: this.props.history,
+    })
+  }
+  !this.state.verifiedToken ? this.verifyToken() : ""
 
-		const {
-			settings,
-			forgotPasswordProperties,
-			resetPasswordProperties
-		} = this.props.state;
+  const {
+    settings,
+    forgotPasswordProperties,
+    resetPasswordProperties,
+  } = this.props.state
 
-		if (
-			this.props.location.search === '' ||
-			this.props.location.search.indexOf('?token=') === -1 ||
-			(resetPasswordProperties && !resetPasswordProperties.status)
-		) {
-			return (
-				<Redirect
-					to={{
-						pathname: '/'
-					}}
-				/>
-			);
-		}
+  if (
+    this.props.location.search === "" ||
+    this.props.location.search.indexOf("?token=") === -1 ||
+    (resetPasswordProperties && !resetPasswordProperties.status)
+  ) {
+    return (
+      <Redirect
+        to={{
+          pathname: "/",
+        }}
+      />
+    )
+  }
 
-		const {
-			checkoutInputClass = 'checkout-field',
-			checkoutButtonClass = 'checkout-button'
-		} = themeSettings;
+  const {
+    checkoutInputClass = "checkout-field",
+    checkoutButtonClass = "checkout-button",
+  } = themeSettings
 
-		return (
-			<>
-				{resetPasswordProperties && (
-					<ResetPassword
-						inputClassName={checkoutInputClass}
-						buttonClassName={checkoutButtonClass}
-						settings={settings}
-						forgotPasswordProperties={forgotPasswordProperties}
-						resetPasswordProperties={resetPasswordProperties}
-						onSubmit={this.handleFormSubmit}
-					/>
-				)}
-			</>
-		);
-	}
+  return (
+    <>
+      {resetPasswordProperties && (
+        <ResetPassword
+          inputClassName={checkoutInputClass}
+          buttonClassName={checkoutButtonClass}
+          settings={settings}
+          forgotPasswordProperties={forgotPasswordProperties}
+          resetPasswordProperties={resetPasswordProperties}
+          onSubmit={this.handleFormSubmit}
+        />
+      )}
+    </>
+  )
+}
 
 export default ResetPasswordForm
--- src/components/theme/src/containers/resetPassword.tsx
+++ src/components/theme/src/containers/resetPassword.tsx
@@ -1,13 +1,7 @@
 import React from "react"
-import { themeSettings, text } from "../lib/settings"
-import MetaTags from "../components/metaTags"
 import ResetPassword from "../components/resetPassword/index"
 
 const ResetPasswordContainer = props => {
-  const {
-    state: { pageDetails, loginUser },
-  } = props
-
   return (
     <section className="section">
       <div className="container">
