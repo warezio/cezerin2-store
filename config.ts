db2ac26b1b26d8664c06493ae0c79aca78d48272
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-04-29 10:55:17 +0700

    Adding config file

---
+++ config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  // meta details
+  title: `Cezerin Store`,
+  description: `This is the Cezerin2 Store`,
+  // mobile details
+  name: `Cezerin Store`,
+  short_name: "Store",
+}
